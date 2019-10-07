7fdc455b27689a6ba486a5576c9563cdf15e3151
Alberto <alberto.mancino94@gmail.com>
2019-10-07 19:46:03 +0200

    Ho spostato solution print dal main a Tree Node

--- Uniformed Search Strategy/DepthLimitedSearch/Main.py
+++ Uniformed Search Strategy/DepthLimitedSearch/Main.py
@@ -2,33 +2,6 @@ import TreeNode as TN
 import Problem as P
 import Problem_State as PS
 
-def Print_Path(node):
-    list = []   # lista dove memorizzare il nome dei nodi da stampare a video
-    temp = node # (node contiene informazioni utili sulla depth e path cost del GOAL node)
-    flag = 0    # flag per il controllo del ciclo: vale 1 se trovo il nodo root
-    print('')
-
-    # Check if the solution is Failed
-    if node == 1:
-        print("Failed to find a valid solution!")
-        return 0
-
-    while 1:
-        list.insert(0,temp)  # salvo in una lista i nodi del percorso: li inserisco all'indice 0 in modo da ottenere un ordine dal nodo iniziale a quello finale
-        if temp.parent == None: # sono arrivato al nodo root
-            flag = 1
-        else:
-            temp = temp.parent # passo al nodo padre per la prossima iterazione
-        if flag :
-            print('--- Path Finder ---')
-            print('I found a valid solution! \nPath Cost =', node.path_cost,'\nTree Depth =', node.depth)
-            #output = ''
-            print('')
-            for index, x in enumerate(list):
-                if index != 0:
-                    print('\t↓\n', x.action,'\n\t↓')
-                PS.print_matrix(x.state.matrix,3,3)
-            return 0
 
 if __name__ == '__main__':
 
@@ -40,8 +13,7 @@ if __name__ == '__main__':
     depth_limit = 10
     problem = P.Problem(start)
 
-    Print_Path(TN.Tree_Search(problem, depth_limit))  #run the search Alg
-
+    TN.Print_Path(TN.Tree_Search(problem, depth_limit))  #run dell'algoritmo di searching
 
 
 
--- Uniformed Search Strategy/DepthLimitedSearch/TreeNode.py
+++ Uniformed Search Strategy/DepthLimitedSearch/TreeNode.py
@@ -71,3 +71,31 @@ def Tree_Search(problem, l):  # l is the depth limit for the Depth Limited Searc
             for node in new_fringe_nodes:
                 Fringe.add(node)
 
+
+def Print_Path(node):
+    list = []   # lista dove memorizzare il nome dei nodi da stampare a video
+    temp = node # (node contiene informazioni utili sulla depth e path cost del GOAL node)
+    flag = 0    # flag per il controllo del ciclo: vale 1 se trovo il nodo root
+    print('')
+
+    # Check if the solution is Failed
+    if node == 1:
+        print("Failed to find a valid solution!")
+        return 0
+
+    while 1:
+        list.insert(0,temp)  # salvo in una lista i nodi del percorso: li inserisco all'indice 0 in modo da ottenere un ordine dal nodo iniziale a quello finale
+        if temp.parent == None: # sono arrivato al nodo root
+            flag = 1
+        else:
+            temp = temp.parent # passo al nodo padre per la prossima iterazione
+        if flag :
+            print('--- Path Finder ---')
+            print('I found a valid solution! \nPath Cost =', node.path_cost,'\nTree Depth =', node.depth)
+            #output = ''
+            print('')
+            for index, x in enumerate(list):
+                if index != 0:
+                    print('\t↓\n', x.action,'\n\t↓')
+                PS.print_matrix(x.state.matrix,3,3)
+            return 0
