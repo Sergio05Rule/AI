a30cbf15305eca9de53572cd4c7ec4ee00293bac
Alberto <alberto.mancino94@gmail.com>
2019-10-07 22:27:09 +0200

    Sistemato Deep Limited e Iterative. Aggiunto tempo impiegato.

--- Uniformed Search Strategy/DepthLimitedSearch/Main.py
+++ Uniformed Search Strategy/DepthLimitedSearch/Main.py
@@ -1,7 +1,7 @@
 import TreeNode as TN
 import Problem as P
 import Problem_State as PS
-
+import time
 
 if __name__ == '__main__':
 
@@ -10,11 +10,10 @@ if __name__ == '__main__':
          [4, 6, 3],
          [0, 7, 8]]
     start = PS.State(r)
+    depth_limit = 9
     problem = P.Problem(start)
 
-    TN.Print_Path(TN.Iterative_Tree_Search(problem))  #run dell'algoritmo di searching
-
-
+    TN.Print_Path(TN.Tree_Search(problem, depth_limit), time.process_time())  #run dell'algoritmo di searching
 
 
 
--- Uniformed Search Strategy/IterativeDeepningSearch/Main.py
+++ Uniformed Search Strategy/IterativeDeepningSearch/Main.py
@@ -1,7 +1,7 @@
 import TreeNode as TN
 import Problem as P
 import Problem_State as PS
-
+import time
 
 if __name__ == '__main__':
 
@@ -10,10 +10,9 @@ if __name__ == '__main__':
          [4, 6, 3],
          [0, 7, 8]]
     start = PS.State(r)
-    depth_limit = 10
     problem = P.Problem(start)
 
-    TN.Print_Path(TN.Tree_Search(problem, depth_limit))  #run dell'algoritmo di searching
+    TN.Print_Path(TN.Iterative_Tree_Search(problem), time.process_time())  #run dell'algoritmo di searching
 
 
 
