f74c5ca9cc4ce01e5a7a2b0cf5008cb0ff3701b9
kur0bi <alberto.mancino94@gmail.com>
2019-10-08 14:07:51 +0200

    Non ho fatto niente

---
+++ Uniformed Search Strategy/DepthLimitedSearchClosed/Main.py
@@ -0,0 +1,20 @@
+import TreeNode as TN
+import Problem as P
+import Problem_State as PS
+import time
+
+if __name__ == '__main__':
+
+    # inizializzazione matrice
+    r = [[5, 1, 2],
+         [4, 6, 3],
+         [0, 7, 8]]
+    start = PS.State(r)
+    depth_limit = 9
+    problem = P.Problem(start)
+
+    TN.Print_Path(TN.Tree_Search(problem, depth_limit), time.process_time())  #run dell'algoritmo di searching
+
+
+
+
