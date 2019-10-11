4b59578854e217e603c3b813cce482ec7366f664
Alberto <alberto.mancino94@gmail.com>
2019-10-11 13:56:01 +0200

    Quasi finito

---
+++ GraphComplete/Problem_State.py
@@ -0,0 +1,18 @@
+
+class State:
+    def __init__(self, id):
+        self.id = id
+
+
+def NotinClosed(problem, node): #restituisce 1 se lo stato non è stato già visitato (al netto di controlli sulla depth) è quindi bisogna aggiungerlo
+    NotVisited = 1
+
+    for element in problem.closed:
+
+        #if node.state.id == tuple[0].id and node.depth >= tuple[1]:
+         #   NotVisited = 0 #presente nei visited ma selected_node ha maggiore/uguale depth
+
+        if node.state.id == element.state.id:
+            NotVisited = 0
+
+    return NotVisited
